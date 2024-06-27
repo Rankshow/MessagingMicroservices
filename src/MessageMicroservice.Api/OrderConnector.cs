a94ed39d1368ca188d6bbe80ae092af51d18cdf3
Rankshow <uwaomaobinna20@gmail.com>
2024-06-27 15:39:04 +0100

    Created the OrderConnector class that implements the interface

---
+++ src/MessageMicroservice.Api/OrderConnector.cs
@@ -0,0 +1,16 @@
+﻿
+namespace MessageMicroservice.Api
+{
+    public class OrderConnector : IOrderConnector
+    {
+        public Task<OrderInfo> GetNextOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveOrder(OrderInfo order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
